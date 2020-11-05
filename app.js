3df7ccb09636a763fc1db3986a125a5ed343842a
Naveen Gaur <naveensgour@gmail.com>
2020-11-05 14:25:13 +0530

    Home page added.

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
-var app = require("express")();
+var express = require("express");
 var bodyParse = require("body-parser");
+var app = express();
+
+app.set('view engine','ejs');
+app.use(express.static("public"));
+app.use(express.static("assets"));
+// app.use('/api', api); // redirect API calls
+// app.use('/', express.static(__dirname + '/www')); // redirect root
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/js', express.static(__dirname + '/node_modules/popper.js/dist')); // redirect popper.js jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
+// ==========================
+// ******* ROUTES************
+// ==========================
 
 
-
+// Home Page
 app.get("/",function(req,res){
-    res.send("Hi from Naveen Gaur.");
+    res.render("Home");
 });
+
+
+
+
+
+
+
 
 
 var port = 3000 ;
